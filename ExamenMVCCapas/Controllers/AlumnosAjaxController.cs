7c3d0d926e90765fc8bd8ef3c9a6c7eb62a079cf
atusparia <rzcuevasm@gmail.com>
2025-02-05 18:01:34 -0500

    completo el crear alumno

---
+++ ExamenMVCCapas/Controllers/AlumnosAjaxController.cs
@@ -0,0 +1,45 @@
+﻿using ExamenMVCCapas.Models;
+using Microsoft.AspNetCore.Mvc;
+using Services;
+
+namespace MVCCapasJQueryAjax.Controllers
+{
+    public class AlumnosAjaxController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Get()
+        {
+            AlumnoService service = new AlumnoService();
+
+            var alumnos = service.Get();
+
+            var model = alumnos.Select(x => new AlumnoModel
+            {
+                AlumnoId = x.AlumnoId,
+                Nombre = x.Nombre,
+                Apellido = x.Apellido,
+                FechaNacimiento = x.FechaNacimiento,
+                CorreoElectronico = x.CorreoElectronico
+            }).ToList();
+
+            return Json(model);
+        }
+
+        [HttpPost]
+        public JsonResult Create(string nombre, string apellido, DateTime fechaNacimiento, string correoElectronico) 
+        {
+            var service= new AlumnoService();   
+            service.Insert(new Domain.Alumno { Nombre = nombre, 
+                Apellido=apellido, 
+                FechaNacimiento= fechaNacimiento, 
+                CorreoElectronico= correoElectronico, 
+                Estado=true });
+
+            return Json(new { message = "Formulario enviado con éxito. Gracias!" });
+        }
+    }
+}
